a3677a1e70275798ed6fd4d80ad776330572387a
Wang Han <416810799@qq.com>
2025-01-27 19:35:33 +0800

    Clean up umount targets (https://github.com/tiann/KernelSU/pull/2386)
    -move /system_ext after /product (ksu_handle_setuid)
    
    -https://github.com/tiann/KernelSU

--- kernel/core_hook.c
+++ kernel/core_hook.c
@@ -1299,14 +1299,13 @@ do_umount:
 	// filter the mountpoint whose target is `/data/adb`
 	try_umount("/odm", true, 0);
 	try_umount("/system", true, 0);
-	try_umount("/system_ext", true, 0);
 	try_umount("/vendor", true, 0);
 	try_umount("/product", true, 0);
+	try_umount("/system_ext", true, 0);
 	try_umount("/data/adb/modules", false, MNT_DETACH);
 
 	// try umount ksu temp path
 	try_umount("/debug_ramdisk", false, MNT_DETACH);
-	try_umount("/sbin", false, MNT_DETACH);
 
 	// try umount hosts file
 	try_umount("/system/etc/hosts", false, MNT_DETACH);
