f90f7705efaac673b2cd81e1197bcbe2889fe05b
sidex15 <24408329+sidex15@users.noreply.github.com>
2025-06-16 18:42:46 +0800

    kernel: implement susfs v1.5.8 KernelSU Next v1.0.8
    -remove some extra spaces from (TG) patch
    -try_umount -> ksu_try_umount if not CONFIG_KSU_SUSFS_TRY_UMOUNT (ksu_handle_setuid)
    
    -7/10/25: selinux/rules: fix merge conflict with previous change (Fix illegal RCU lock usage):
    __https://github.com/pershoot/KernelSU-Next/commit/4d633a1e0e5934148b0de998b06c16e4ae2f0f74
    -7/14/25: core_hook: fix merge conflict with previous change (remove redundant get_cred_rcu):
    __https://github.com/pershoot/KernelSU-Next/commit/bd03c296a1f2af9569dfbb49077068fef1864c9d
    -7/20/25: ksud: fix merge conflict with previous change (guard syscall hook types):
    __https://github.com/pershoot/KernelSU-Next/commit/45ad73e9dd86a0ff04a02e73a8fc2dbc3160ee6c
    -7/20/26: core_hook: fix merge conflict with previous change (support for KernelNoSU):
    __https://github.com/pershoot/KernelSU-Next/commit/948975ba35bebbd6e4b4ab41d33715ce7d79b572
    -7/24/25: core_hook: fix merge conflict with previous change (allow only perms ... and guard kernelnosu perms):
    __https://github.com/pershoot/KernelSU-Next/commit/3f4c23a34f4f2d3011a0bb0b6a14a8f10bd18b7d
    -8/10/25: Kconfig: fix merge conflict with previous change (nest ksun switch manager support)
    __https://github.com/pershoot/KernelSU-Next/commit/e0c461322bca8ab698e3e9749a7d467f67f5daa4
    -9/30/25: Kconfig / apk_sign: fix merge conflict with previous change (remove KSU_SWITCH_MANAGER logic)
    __https://github.com/pershoot/KernelSU-Next/commit/26ee31e01c6cb8d4f68f673009bf6287acd439f2
    -10/3/25: core_hook: fix merge conflict with previous change (harden prctl check)
    __https://github.com/pershoot/KernelSU-Next/commit/f1c97b507efe439906f49d5f33b8c7ddebe5be71
    __Add on a check for current_ui_val to discern jump to skip_check
    ____Seperated out this change and placed it right before this one
    __'kernel: core_hook: harden prctl handler', https://github.com/backslashxx/KernelSU
    -10/5/25: core_hook: fix merge conflict with previous change (align prct harden commit from backslashx)
    __https://github.com/pershoot/KernelSU-Next/commit/478394a6e1ce916c7dab3370a3aeae69fa775d41
    ____Revert: 'Add on a check for current_ui_val to discern jump to skip_check'
    ____Add: 'kernel: Change ordering to discern skip_check (crowned after private spaces), prior to current_uid_val eval'
    ______(right before this one)
    -10/6/25: core_hook: fix merge conflict with previous changes (reverts (2) of prctl harden)
    __https://github.com/pershoot/KernelSU-Next/commit/11643ee685cbac28f995319790a9adb86d29558f
    __https://github.com/pershoot/KernelSU-Next/commit/69be375db10a98c89d480108b539cbeac1994e54
    ____Revert: 'kernel: Change ordering to discern skip_check (crowned after private spaces), prior to current_uid_val eval'
    -10/19/25: core_hook: fix indentation (use tabs instead of spaces (ksu_handle_prctl))
    
    -https://gitlab.com/simonpunk/susfs4ksu/-/tree/gki-android14-6.1

--- kernel/apk_sign.c
+++ kernel/apk_sign.c
@@ -317,8 +317,24 @@ module_param_cb(ksu_debug_manager_uid, &expected_size_ops,
 
 #endif
 
-bool is_manager_apk(char *path)
+bool ksu_is_manager_apk(char *path)
 {
+	int tries = 0;
+
+	while (tries++ < 10) {
+		if (!is_lock_held(path))
+			break;
+
+		pr_info("%s: waiting for %s\n", __func__, path);
+		msleep(100);
+	}
+
+	// let it go, if retry fails, check_v2_signature will fail to open it anyway
+	if (tries == 10) {
+		pr_info("%s: timeout for %s\n", __func__, path);
+		return false;
+	}
+
 	// set debug info to print size and hash to kernel log
 	pr_info("%s: expected size: %u, expected hash: %s\n",
 		path, expected_manager_size, expected_manager_hash);
